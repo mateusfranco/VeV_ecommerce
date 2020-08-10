f2229e0e620e56d8ce686a89d23ee442a318691b
Mateus Franco <mateusfrancofs3@gmail.com>
2020-08-10 09:45:48 -0300

    init with products

---
+++ app/src/Products/Products.js
@@ -0,0 +1,55 @@
+const { uuid } = require("uuidv4");
+const router = require('express').Router();
+
+const {
+  products
+} = require('../../db/db');
+
+router.get("/products", (req, res) => {
+    const { title } = req.query;
+    const results = title
+      ? products.filter((product) => product.title.includes(title))
+      : products;
+  
+    return res.json(results);
+  });
+  
+router.post("/products", (req, res) => {
+  console.log(req, res);
+  const { title, price, description, images, quantity } = req.body;
+  const product = {
+    id: uuid(),
+    title,
+    price,
+    description,
+    images,
+    quantity,
+  };
+  products.push(product);
+  return res.json(product);
+});
+
+router.put("/products/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, price, description, images, quantity } = req.body;
+
+  const productIndex = products.findIndex((product) => product.id === id);
+  if (productIndex < 0) {
+    return res.status(400).json({ error: "Product not found!" });
+  }
+
+  const product = {
+    id,
+    title,
+    price,
+    description,
+    images,
+    quantity,
+  };
+
+  product[productIndex] = product;
+
+  return response.json(product);
+});
+
+module.exports = router;
