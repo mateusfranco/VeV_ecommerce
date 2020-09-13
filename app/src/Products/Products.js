ce10629e08b78663c59ad7d82a25ba8463d7d85f
Mateus Franco <mateusfrancofs3@gmail.com>
2020-09-13 20:48:53 -0300

    install mongo completed

--- app/src/Products/Products.js
+++ app/src/Products/Products.js
@@ -9,15 +9,15 @@ router.get("/products",async (req, res) => {
     ? await db.collection('Products').find(product => {
       return product.title === title;
     }).toArray()
-    : await db.collection('Products').find().toArray();    
-    return res.json(results);
+    : await db.collection('Products').find().toArray();
+    return res.status(200).json(results);
 });
 
 router.post("/products",async (req, res) => {
   
   const { title, price, description, images, quantity } = req.body;
   const product = {
-    _id: uuid(),
+    id: uuid(),
     title,
     price,
     description,
@@ -42,17 +42,17 @@ router.put("/products/:id",async (req, res) => {
   };
 
   const result = await db.collection('Products')
-    .findOneAndReplace({'_id':id},
+    .findOneAndReplace({'id':id},
     product);
   if(!result.lastErrorObject.updatedExisting) return res.status(400)
     .json({message:'object dont exists'}); 
-  return res.json(product);
+  return res.status(200).json(product);
 });
 
 router.delete("/products/:id", async (req, res) =>{
   const {id} = req.params;
   const result = await db.collection('Products')
-    .findOneAndDelete({'_id':id});
+    .findOneAndDelete({'id':id});
   if(!result.value) return res.status(400).send();
   return res.status(204).send();
 });
--- app/src/Products/Products.test.js
+++ app/src/Products/Products.test.js
@@ -1,82 +1,83 @@
-const { uuid } = require("uuidv4");
 const dotenv = require('dotenv');
 dotenv.config();
-const dbPath = process.env.DB_PRODUCTION_URL;
-const request = require("supertest");
-const app = require("../../../main");
 const {Products} = require('../Products/ProductsSchema');
+const mongoose = require('mongoose');
 
-describe("products", async () => {
-  // beforeAll(async () => {
-  //   const db = await mongoose.connect(dbPath, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
-  //       if (err) {
-  //           console.error(err);
-  //           process.exit(1);
-  //       }
-  //   });
-  // });
+describe("products", () => {
+
+  beforeAll(async () => {
+    await mongoose.connect(process.env.DB_PRODUCTION_URL, { useNewUrlParser: true, useCreateIndex: true }, (err) => {
+      if (err) {
+          console.error(err);
+          process.exit(1);
+      }
+    });
+  });
   
+  beforeEach((done) => {
+    const newMockProduct = {
+      id: "asdg 872613 ajhds",
+      title: "testePost1",
+      price: 123.45,
+      description: "testepost1",
+      images: "images",
+      quantity: 20,
+    };
+
+    prod = new Products(newMockProduct);
+    prod.save()
+      .then(() => done());
+  });
+
+  it('teste', () => {
+    expect(true).toBe(true);
+  });
 
   it("should get all products", async () => {
     const prod = await Products.find();
-    console.log(prod);
-    // const res = await jest.fn().mockResolvedValue([
-    //   {
-    //     "_id": "fe59bb95-ce73-45dd-a889-096ea4daf153",
-    //     "title": "teste",
-    //     "price": 1000.40,
-    //     "description": "Headset gamer razer",
-    //     "images": "asjdhgasdhgdgajhsdgjhasdghagjhasgd",
-    //     "quantity": 10
-    //   }
-    // ])
+    const prodCount = await Products.count();
     
-    // expect(res.statusCode).toEqual(200);
+    expect(prod).toHaveLength(prodCount);
   });
 
-  // it("should get products array", async () => {
-  //   const res = await request(app).get("/products");
-  //   expect(res.statusCode).toEqual(200);
-  // })
-
-  // it("should create a new product", async () => {
-  //   const dbProductsLengthBefore = db.products.length;
-  //   const newMockProduct = {
-  //     title: "testePost",
-  //     price: 123.32,
-  //     description: "testepost",
-  //     images: "jadghsdgjsdgajhsdgjhasgdjhasgdjhgasjdhgasdjhgasjdjasdad",
-  //     quantity: 21,
-  //   };
-  //   const res = await request(app).post("/products").send(newMockProduct);
-  //   expect(res.statusCode).toEqual(200);
-  //   const dbProductsLengthAfter = db.products.length;
-  //   expect(dbProductsLengthBefore).toEqual(dbProductsLengthAfter - 1);
-  // });
-
-  // it("alter one product", async () => {
-  //   const productId = db.products[0].id;
-  //   const product = {
-  //     id: productId,
-  //     title: "testePost",
-  //     price: 123.32,
-  //     description: "testepost",
-  //     images: "jjjjjjjj",
-  //     quantity: 21,        
-  //   };
-  //   const res = await request(app).put(`/products/${productId}`).send(product);
-  //   expect(res.statusCode).toEqual(200);
-  //   expect(res.body).toEqual(product);
-  //   // verificar se o produto foi alterado 
-  // });
-
-  // it('should delete product id', async () => {
-  //   const dbProductsLengthBefore = db.products.length;
-  //   const productId = db.products[0].id;
-  //   const res = await request(app).delete(`/products/${productId}`);
-  //   expect(res.statusCode).toEqual(204);
-  //   const dbProductsLengthAfter = db.products.length;
-  //   expect(dbProductsLengthBefore - 1).toEqual(dbProductsLengthAfter);
+  it("should create a new product", async (done) => {
     
-  // })
+    const newMockProduct = {
+      id: "asdg 872613 ajhdsgjahsgd",
+      title: "testePost1",
+      price: 123.45,
+      description: "testepost1",
+      images: "images",
+      quantity: 20,
+    };
+
+    const product = new Products(newMockProduct);
+    
+    product
+      .save() //takes some time and returns a promise
+      .then(() => {
+          expect(!product.isNew).toBe(true); //if poke is saved to db it is not new
+          done();
+      });
+  });
+
+  it('should remove a Product', async (done) => {
+    Products.findOneAndRemove({title: "testePostPopulate"})
+      .then(() => Products.findOne({title: "testePostPopulate"}))
+      .then((product) => {
+        expect(product).toBe(null);
+        done();
+      });
+  });
+
+  it('sets and saves pokemon using an instance', (done) => {
+    prod.set('title', 'newtest');
+    prod
+    .save()
+    .then(()=> {
+      expect(prod).toMatchObject({...prod, title:'newtest'})
+      done();
+    });
+  });
+
 });
